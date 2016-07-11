696907a56831cd186f281b59bb116c0cd16be2da
raimonfuns <raimonfuns@163.com>
2016-07-11 15:49:45 +0800

    STATIC

--- Express.js
+++ Express.js
@@ -1,6 +1,11 @@
+var path = require('path')
 var express = require('express')
+
 var app = express()
 var port = process.argv[2]
+var fileDir = process.argv[3] || path.join(__dirname, 'public')
+
+app.use(express.static(fileDir))
 
 app.get('/home', function (req, res) {
   res.end('Hello World!')
